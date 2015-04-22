dfb630dbe17d8331d45ceca5faac4d22572c1794
Miguel Angel Gordian <os.aioria@gmail.com>
2015-04-21 20:09:54 -0500

    Adds the task rmiCompile

--- src/main/scala/sbtrmi/Plugin.scala
+++ src/main/scala/sbtrmi/Plugin.scala
@@ -7,13 +7,16 @@ import Keys._
 object RMIServer extends sbt.Plugin {
   val rmiPort = settingKey[Int]("Port runnning rmiserver.")
   val rmiPath = settingKey[String]("Path where rmiServer will be executed.")
-
+  val rmiClass = settingKey[String]("Classes to compile with rmiserver")
   val rmiStart = taskKey[Unit]("really wake up the rmi server")
   val rmiStop = taskKey[Unit]("stop the rmi server")
+  val rmiCompile = taskKey[Unit]("Compile with rmic")
+
 
   val rmiSettings = Seq(
      rmiPort := 8081,
      rmiPath := "/target/scala-2.10/",
+     rmiClass := "Server",
      rmiStart := {
        println("despertando a rmi!" )
       sys.process.Process(Seq("rmiregistry", rmiPort.value.toString ), new java.io.File("/home/zoek/workspace/scala/sbt-rmi/target/scala-2.10/sbt-0.13/classes/sbtrmi")).!
@@ -22,6 +25,12 @@ object RMIServer extends sbt.Plugin {
          import scala.sys.process._
         "killall rmiregistry".!!
 	       println("matando a todos los rmi >.<!")
+     },
+     rmiCompile := {
+       val scalaVer = "scala-" + scalaVersion.value.toString.replaceAll("\\.\\d+$", "")
+       val sbtVer = "sbt-" + sbtVersion.value.toString.replaceAll("\\.\\d+$", "")
+       val classes = file(s"./target/${scalaVer}/${sbtVer}") ** "*.class"
+       sys.process.Process(Seq("rmic", rmiClass.value.toString  + ".class"), new java.io.File(s"./target/${scalaVer}/${sbtVer}/classes/sbtrmi")).!
      }
   )
 
@@ -48,6 +57,9 @@ object RMIServer extends sbt.Plugin {
 
     // get name of current project
     val nameOpt: Option[String] = name in currentRef get structure.data
+
+
+
 
     println("wake up! " + nameOpt)
     state
